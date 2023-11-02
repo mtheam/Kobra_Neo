d4c79b8d4af2d32685bf8c14022913e84034a028
U. D <1coderookie@quantentunnel.de>
2023-11-02 14:29:40 +0100

    Update Configuration.h
    Changed probe's y-offset from 0 to +4

--- source/Marlin/Configuration.h
+++ source/Marlin/Configuration.h
@@ -1018,7 +1018,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 37.5,0, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 37.5,4, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
